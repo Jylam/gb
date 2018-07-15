134a8002797971cae82f8698bb1637be752f19b2
Jylam <jylam@lnxscene.org>
2018-07-16 00:01:37 +0200

    16bits registers accessors

--- hestkuk/src/lr35902.rs
+++ hestkuk/src/lr35902.rs
@@ -10,6 +10,8 @@ struct Opcode {
     jump: bool,
 }
 
+#[allow(dead_code)]
+#[derive(Copy, Clone)]
 pub struct Registers {
     A: u8,
     B: u8,
@@ -22,14 +24,39 @@ pub struct Registers {
     PC: u16,
     SP: u16,
 }
+#[allow(dead_code)]
+impl Registers {
+    fn get_AF(self) -> u16 {
+       ((self.A as u16)<<8) | ((self.F as u16)&0xFF)
+    }
+    fn set_AF(mut self, v: u16) {
+        self.A = ((v&0xFF00)>>8) as u8;
+        self.F = (v&0xFF) as u8;
+    }
+    fn get_BC(self) -> u16 {
+       ((self.B as u16)<<8) | ((self.C as u16)&0xFF)
+    }
+    fn set_DE(mut self, v: u16) {
+        self.D = ((v&0xFF00)>>8) as u8;
+        self.E = (v&0xFF) as u8;
+    }
+    fn get_HL(self) -> u16 {
+       ((self.H as u16)<<8) | ((self.L as u16)&0xFF)
+    }
+    fn set_HL(&mut self, v: u16) {
+        println!("Setting HL to {:04X}...", v);
+
+        self.H = (((v&0xFF00)as u16)>>8) as u8;
+        self.L = (v&0xFF) as u8;
+    }
+}
 
 // Sharp LR35902 CPU emulator
 pub struct Cpu<'a> {
     mem: mem::Mem<'a>,
-    PC: u16,
+    regs: Registers,
     total_cyles: u64,
     opcodes: Vec<Opcode>,
-    A: u8,
 }
 
 pub fn addr16(cpu: &mut Cpu) -> u16 {
@@ -49,15 +76,17 @@ pub fn NOP(_cpu: &mut Cpu) {
 }
 pub fn XORa(cpu: &mut Cpu) {
     println!("XOR a");
-    cpu.A = cpu.A^cpu.A;
+    cpu.regs.A = cpu.regs.A^cpu.regs.A;
 }
 pub fn LDhld16(cpu: &mut Cpu) {
     let imm = imm16(cpu);
+    cpu.regs.set_HL(imm);
     println!("LD HL, {:04X}", imm)
+
 }
 pub fn JPa16(cpu: &mut Cpu) {
     let addr = addr16(cpu);
-    cpu.PC = addr;
+    cpu.regs.PC = addr;
     println!("JP {:04X}", addr)
 }
 impl<'a> Cpu<'a>{
@@ -65,8 +94,18 @@ impl<'a> Cpu<'a>{
     pub fn new(mem: mem::Mem) -> Cpu {
         let mut cpu: Cpu;
         cpu = Cpu{
-            PC: 0x100,
-            A: 0x00,
+            regs: Registers {
+                A: 0,
+                B: 0,
+                D: 0,
+                H: 0,
+                F: 0,
+                C: 0,
+                E: 0,
+                L: 0,
+                PC: 0,
+                SP: 0,
+            },
             mem: mem,
             total_cyles: 0,
             opcodes:
@@ -84,7 +123,7 @@ impl<'a> Cpu<'a>{
             len: 1,
             cycles: 4,
             execute: NOP,
-                    jump: false,
+            jump: false,
         };
         cpu.opcodes[0x21] = Opcode {
             name: "LDhld16",
@@ -105,34 +144,37 @@ impl<'a> Cpu<'a>{
             len: 3,
             cycles: 16,
             execute: JPa16,
-                    jump: true,
+            jump: true,
         };
         cpu
     }
     pub fn get_PC(&self) -> u16 {
-        self.PC
+        self.regs.PC
     }
 
     pub fn print_status(&self) {
         println!("==== CPU ====");
         println!("PC: {:04X}", self.get_PC());
-        println!("A : {:02X}", self.A);
+        println!("A : {:02X}\tF : {:02X}", self.regs.A, self.regs.F);
+        println!("B : {:02X}\tC : {:02X}", self.regs.B, self.regs.C);
+        println!("D : {:02X}\tE : {:02X}", self.regs.D, self.regs.E);
+        println!("H : {:02X}\tL : {:02X}", self.regs.H, self.regs.L);
         println!("==== END ====");
     }
 
     pub fn reset(&mut self) {
-        self.PC = 0x100
+        self.regs.PC = 0x100
     }
 
     pub fn step(&mut self) {
-        let code = self.mem.read8(self.PC) as usize;
+        let code = self.mem.read8(self.regs.PC) as usize;
         let opcode = self.opcodes[code];
         let _name  = opcode.name;
-        print!("{:04X}: {:02X} -> ", self.PC, code);
+        print!("{:04X}: {:02X} -> ", self.regs.PC, code);
         (opcode.execute)(self);
         self.total_cyles = self.total_cyles + opcode.cycles as u64;
         if !opcode.jump {
-            self.PC = self.PC.wrapping_add(opcode.len);
+            self.regs.PC = self.regs.PC.wrapping_add(opcode.len);
         }
     }
 
